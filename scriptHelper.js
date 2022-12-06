a4b5a23d8d9fdee7de7dafbf1507762ec256b881
Jonathan Cruzen <jonathancruzen@gmail.com>
2022-12-05 18:27:49 -0600

    fixed even more spelling in scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -53,7 +53,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
      } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
         fuel.innerHTML = " Fuel level high enough for launch";
         cargo.innerHTML = "Cargo mass too heavy for launch";
-        launchStatus.innerHTML = "Shuttle Not Ready for Launch.";
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = "rgb(199, 37, 78)";
      } else if (fuelLevel < 10000 && cargoLevel > 10000) {
         fuel.innerHTML = "Fuel level too low for launch";
@@ -62,7 +62,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         launchStatus.style.color = "rgb(199, 37, 78)";
      } else {
         fuel.innerHTML = "Fuel level high enough for launch";
-        cargo.innerHTML = "Cargo mass is low enough for launch";
+        cargo.innerHTML = "Cargo mass low enough for launch";
         launchStatus.innerHTML = "Shuttle is Ready for Launch";
         launchStatus.style.color = "rgb(65, 159, 106)";
      }
